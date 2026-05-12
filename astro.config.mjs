23dfd155173088c772268391853ab96e161cf3c5
Maja Estrup <maes0002@stud.kea.dk>
2026-05-12 18:05:28 +0200

    remove error

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import alpinejs from "@astrojs/alpinejs";
-import node from "@astrojs/node";
 
 export default defineConfig({
   output: "static",
-  adapter: node({ mode: "standalone" }),
   vite: {
     plugins: [tailwindcss()],
   },
